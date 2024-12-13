e00c41a05757e824b84bfd8605694856926d5c16
Esam <smsm22.smesm@gmail.com>
2024-12-13 02:10:57 +0200

    finish task

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -1,9 +1,6 @@
-import 'package:fintech_challenge/features/graph/cubit/graph_cubit.dart';
 import 'package:fintech_challenge/main.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../data/repo/orders_repo.dart';
 import '../../features/graph/view/graph_page.dart';
 import '../../features/metrics/view/metrics_page.dart';
 
@@ -19,13 +16,7 @@ class AppRouter {
       case metricsPage:
         return MaterialPageRoute(builder: (_) => const MetricsPage());
       case graphPage:
-        return MaterialPageRoute(
-            builder: (_) => BlocProvider(
-                  create: (context) => GraphCubit(OrdersRepoImpl())
-                    ..fetchOrders()
-                    ..fetchOrders(),
-                  child: const GraphPage(),
-                ));
+        return MaterialPageRoute(builder: (_) => const GraphPage());
     }
     return null;
   }
--- lib/features/graph/view/graph_page.dart
+++ lib/features/graph/view/graph_page.dart
@@ -6,28 +6,33 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../data/repo/orders_repo.dart';
+
 class GraphPage extends StatelessWidget {
   const GraphPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(StringsManager.ordersOverTime),
-        automaticallyImplyLeading: !kIsWeb,
-      ),
-      backgroundColor: ColorManager.itemBG,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: BlocBuilder<GraphCubit, GraphState>(
-          builder: (context, state) {
-            final ordersData =
-                context.read<GraphCubit>().getOrderCountsByDate();
-            if (state is OrdersLoading) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            return BarChartWidget(ordersData: ordersData);
-          },
+    return BlocProvider(
+      create: (context) => GraphCubit(OrdersRepoImpl())..fetchOrders(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(StringsManager.ordersOverTime),
+          automaticallyImplyLeading: !kIsWeb,
+        ),
+        backgroundColor: ColorManager.itemBG,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: BlocBuilder<GraphCubit, GraphState>(
+            builder: (context, state) {
+              final ordersData =
+                  context.read<GraphCubit>().getOrderCountsByDate();
+              if (state is OrdersLoading) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return BarChartWidget(ordersData: ordersData);
+            },
+          ),
         ),
       ),
     );
