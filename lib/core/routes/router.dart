0d9b63f64270dc9dbade6e3859fdee2e3b027fc2
Esam <smsm22.smesm@gmail.com>
2024-12-13 02:10:04 +0200

    finish task

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -5,7 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../data/repo/orders_repo.dart';
 import '../../features/graph/view/graph_page.dart';
-import '../../features/metrics/cubit/metrics_cubit.dart';
 import '../../features/metrics/view/metrics_page.dart';
 
 class AppRouter {
@@ -18,12 +17,7 @@ class AppRouter {
       case homePage:
         return MaterialPageRoute(builder: (_) => const MyHomePage());
       case metricsPage:
-        return MaterialPageRoute(
-            builder: (_) => BlocProvider(
-                  create: (context) =>
-                      MetricsCubit(OrdersRepoImpl())..fetchOrders(),
-                  child: const MetricsPage(),
-                ));
+        return MaterialPageRoute(builder: (_) => const MetricsPage());
       case graphPage:
         return MaterialPageRoute(
             builder: (_) => BlocProvider(
--- lib/features/metrics/view/metrics_page.dart
+++ lib/features/metrics/view/metrics_page.dart
@@ -7,51 +7,56 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../data/repo/orders_repo.dart';
+
 class MetricsPage extends StatelessWidget {
   const MetricsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(StringsManager.metricsOverview),
-        automaticallyImplyLeading: !kIsWeb,
-      ),
-      backgroundColor: ColorManager.itemBG,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: BlocBuilder<MetricsCubit, MetricsState>(
-          builder: (context, state) {
-            if (state is OrdersLoading) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            final cubit = context.read<MetricsCubit>();
-            return GridView.extent(
-              padding: const EdgeInsets.all(10),
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              maxCrossAxisExtent: 450,
-              children: [
-                MetricsCard(
-                  title: StringsManager.totalOrders,
-                  value: cubit.totalCount,
-                  imagePath: AssetManager.orders,
-                ),
-                MetricsCard(
-                  title: StringsManager.averagePrice,
-                  value: cubit.averagePrice,
-                  suffix: ' \$',
-                  imagePath: AssetManager.averagePrice,
-                ),
-                MetricsCard(
-                  title: StringsManager.returnedOrders,
-                  value: cubit.returnsCount,
-                  imagePath: AssetManager.returned,
-                ),
-              ],
-            );
-          },
+    return BlocProvider(
+      create: (context) => MetricsCubit(OrdersRepoImpl())..fetchOrders(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(StringsManager.metricsOverview),
+          automaticallyImplyLeading: !kIsWeb,
+        ),
+        backgroundColor: ColorManager.itemBG,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: BlocBuilder<MetricsCubit, MetricsState>(
+            builder: (context, state) {
+              if (state is OrdersLoading) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              final cubit = context.read<MetricsCubit>();
+              return GridView.extent(
+                padding: const EdgeInsets.all(10),
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                maxCrossAxisExtent: 450,
+                children: [
+                  MetricsCard(
+                    title: StringsManager.totalOrders,
+                    value: cubit.totalCount,
+                    imagePath: AssetManager.orders,
+                  ),
+                  MetricsCard(
+                    title: StringsManager.averagePrice,
+                    value: cubit.averagePrice,
+                    suffix: ' \$',
+                    imagePath: AssetManager.averagePrice,
+                  ),
+                  MetricsCard(
+                    title: StringsManager.returnedOrders,
+                    value: cubit.returnsCount,
+                    imagePath: AssetManager.returned,
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
